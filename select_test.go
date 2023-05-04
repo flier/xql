8e5177cec05d7b344963be86055c93ec4de12679
Flier Lu <flier.lu@gmail.com>
2023-05-04 15:17:37 +0800

    use build step as SQL

--- select_test.go
+++ select_test.go
@@ -7,20 +7,20 @@ import (
 )
 
 func ExampleSelect() {
-	fmt.Println(SelectAllFrom(QName("table1")).Query())
-	fmt.Println(Select(Asterisk).From(QName("table1")).Query())
-	fmt.Println(SelectDistinct(Asterisk).From(QName("table1")).Query())
-	fmt.Println(Select(Raw("3 + 4").As("sum")).Query())
+	fmt.Println(SelectAllFrom(QName("table1")))
+	fmt.Println(Select(Asterisk).From(QName("table1")))
+	fmt.Println(SelectDistinct(Asterisk).From(QName("table1")))
+	fmt.Println(Select(Raw("3 + 4").As("sum")))
 
 	var random = Func("random")
-	fmt.Println(Select(random()).Query())
+	fmt.Println(Select(random()))
 
-	fmt.Println(Select(Column("a"), Column("b"), Column("c")).From(QName("table1")).Query())
-	fmt.Println(Select(Column("a").As("value"), Raw("b + c").As("sum")).From(QName("table1")).Query())
+	fmt.Println(Select(Column("a"), Column("b"), Column("c")).From(QName("table1")))
+	fmt.Println(Select(Column("a").As("value"), Raw("b + c").As("sum")).From(QName("table1")))
 
 	var tbl1 = QName("tbl1")
 	var tbl2 = QName("tbl2")
-	fmt.Println(Select(tbl1.Join("a"), tbl2.Join("a"), tbl2.Join("b")).From(tbl1, tbl2).Query())
+	fmt.Println(Select(tbl1.Join("a"), tbl2.Join("a"), tbl2.Join("b")).From(tbl1, tbl2))
 	// Output:
 	// SELECT * FROM table1
 	// SELECT * FROM table1
